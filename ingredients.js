938cfc2d36ce88fac3e642d0778fd55da3b8c8ef
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-03-04 08:49:52 -0700

    More Array examples

--- ingredients.js
+++ ingredients.js
@@ -50,3 +50,38 @@ let range = function (start, end, step) {
 console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
+
+/*
+When this function is given an array and a value, it should return the index 
+of the last time the value occurs in the array. If the value never occurs, the function should return -1.
+*/
+
+let lastIndexOf = function (arr, val) {
+  let lastIndexValue = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      lastIndexValue = i; 
+    }
+  }
+  return lastIndexValue;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+
+
+//Array concatenation example
+let concat = function (arr1, arr2) {
+  for (let i = 0; i < arr2.length; i++) {
+    arr1.push(arr2[i]);
+  }
+  return arr1;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
