3f014df94969e7bdd1bbd36ab1fc20874a1f0b59
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-03-04 08:33:14 -0700

    Range example

--- ingredients.js
+++ ingredients.js
@@ -16,3 +16,37 @@ for (let x = 0; x < ingredients.length; x++) {
 for (let y = (ingredients.length - 1); y >= 0; y--) {
   console.log(ingredients[y]);
 }
+
+/*
+1. The function takes 3 integer parameters: start, end, and step.
+2. The function should return an array of numbers from start to end counting by step.
+3. The function should return an empty array [] if given incorrect parameters, such as:
+
+start, end, or step being undefined
+start being greater than end
+step being 0, or negative
+*/
+let range = function (start, end, step) {
+  let finalArray = [];
+  if (start == null || end == null || step == null || (start > end) || (step <= 0)) {
+    return finalArray;
+  }
+  else {
+    let val = start;
+    finalArray.push(val);
+    let i = start;
+    while (i <= end) {
+      val = val + step;
+      i = val;
+      if (val <= end){
+        finalArray.push(val);
+      }
+      
+    }
+    return finalArray;
+  }
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
